5623cbce19265f7edc0a2ad123133deca3bf98af
Neil Killeen <nkilleen@gmail.com>
2018-04-29 22:08:22 -0700

    + dynamic parallelism with echo 512

--- x16/cuda_x16_echo512.cu
+++ x16/cuda_x16_echo512.cu
@@ -31,74 +31,71 @@ __device__ __forceinline__ void AES_2ROUND(const uint32_t* __restrict__ sharedMe
 	k0++;
 }
 
+__global__ void shift_rows(uint32_t *W) {
+  uint32_t t[4];
+  /// 1, 5, 9, 13
+  t[0] = W[threadIdx.x +  4];
+  t[1] = W[threadIdx.x +  8];
+  t[2] = W[threadIdx.x + 24];
+  t[3] = W[threadIdx.x + 60];
+
+  W[threadIdx.x +  4] = W[threadIdx.x + 20];
+  W[threadIdx.x +  8] = W[threadIdx.x + 40];
+  W[threadIdx.x + 24] = W[threadIdx.x + 56];
+  W[threadIdx.x + 60] = W[threadIdx.x + 44];
+
+  W[threadIdx.x + 20] = W[threadIdx.x + 36];
+  W[threadIdx.x + 40] = t[1];
+  W[threadIdx.x + 56] = t[2];
+  W[threadIdx.x + 44] = W[threadIdx.x + 28];
+
+  W[threadIdx.x + 28] = W[threadIdx.x + 12];
+  W[threadIdx.x + 12] = t[3];
+  W[threadIdx.x + 36] = W[threadIdx.x + 52];
+  W[threadIdx.x + 52] = t[0];
+}
+
+__global__ void mix_columns(uint32_t *W) {
+  uint32_t a[4];
+  int i = blockIdx.x * blockDim.x + threadIdx.x;
+  int j = blockIdx.y * blockDim.y + (threadIdx.y << 4);//* 16;
+
+  a[0] = W[i + j];
+  a[1] = W[i + j + 4];
+  a[2] = W[i + j + 8];
+  a[3] = W[i + j + 12];
+
+  uint32_t ab = a[0] ^ a[1];
+  uint32_t bc = a[1] ^ a[2];
+  uint32_t cd = a[2] ^ a[3];
+
+  uint32_t t, t2, t3;
+  t  = (ab & 0x80808080);
+  t2 = (bc & 0x80808080);
+  t3 = (cd & 0x80808080);
+
+  uint32_t abx = (t  >> 7) * 27U ^ ((ab^t)  << 1);
+  uint32_t bcx = (t2 >> 7) * 27U ^ ((bc^t2) << 1);
+  uint32_t cdx = (t3 >> 7) * 27U ^ ((cd^t3) << 1);
+
+  W[i + j] = bc ^ a[3] ^ abx;
+  W[i + j +  4] = a[0] ^ cd ^ bcx;
+  W[i + j +  8] = ab ^ a[3] ^ cdx;
+  W[i + j + 12] = ab ^ a[2] ^ (abx ^ bcx ^ cdx);
+}
+
 __device__
 static void echo_round(uint32_t* const sharedMemory, uint32_t *W, uint32_t &k0)
 {
-	// Big Sub Words
 	#pragma unroll 16
 	for (int idx = 0; idx < 16; idx++) {
 		AES_2ROUND(sharedMemory, W[(idx << 2) + 0], W[(idx << 2) + 1], W[(idx << 2) + 2], W[(idx << 2) + 3], k0);
 	}
 
-	// Shift Rows
-	#pragma unroll 4
-	for (int i = 0; i < 4; i++)
-	{
-		uint32_t t[4];
-		/// 1, 5, 9, 13
-		t[0] = W[i +  4];
-		t[1] = W[i +  8];
-		t[2] = W[i + 24];
-		t[3] = W[i + 60];
+  shift_rows<<<1, 4>>>(W);
 
-		W[i +  4] = W[i + 20];
-		W[i +  8] = W[i + 40];
-		W[i + 24] = W[i + 56];
-		W[i + 60] = W[i + 44];
-
-		W[i + 20] = W[i + 36];
-		W[i + 40] = t[1];
-		W[i + 56] = t[2];
-		W[i + 44] = W[i + 28];
-
-		W[i + 28] = W[i + 12];
-		W[i + 12] = t[3];
-		W[i + 36] = W[i + 52];
-		W[i + 52] = t[0];
-	}
-
-	// Mix Columns
-	#pragma unroll 4
-	for (int i = 0; i < 4; i++)
-	{
-		#pragma unroll 4
-		for (int idx = 0; idx < 64; idx += 16)
-		{
-			uint32_t a[4];
-			a[0] = W[idx + i];
-			a[1] = W[idx + i + 4];
-			a[2] = W[idx + i + 8];
-			a[3] = W[idx + i + 12];
-
-			uint32_t ab = a[0] ^ a[1];
-			uint32_t bc = a[1] ^ a[2];
-			uint32_t cd = a[2] ^ a[3];
-
-			uint32_t t, t2, t3;
-			t  = (ab & 0x80808080);
-			t2 = (bc & 0x80808080);
-			t3 = (cd & 0x80808080);
-
-			uint32_t abx = (t  >> 7) * 27U ^ ((ab^t)  << 1);
-			uint32_t bcx = (t2 >> 7) * 27U ^ ((bc^t2) << 1);
-			uint32_t cdx = (t3 >> 7) * 27U ^ ((cd^t3) << 1);
-
-			W[idx + i] = bc ^ a[3] ^ abx;
-			W[idx + i +  4] = a[0] ^ cd ^ bcx;
-			W[idx + i +  8] = ab ^ a[3] ^ cdx;
-			W[idx + i + 12] = ab ^ a[2] ^ (abx ^ bcx ^ cdx);
-		}
-	}
+	dim3 threadsPerBlock(4, 4);
+	mix_columns<<<1, threadsPerBlock>>>(W);
 }
 
 __device__ __forceinline__
@@ -112,17 +109,17 @@ void cuda_echo_round_80(uint32_t *const __restrict__ sharedMemory, uint32_t *con
 	h[18] = data[18];
 	h[19] = cuda_swab32(nonce);
 	h[20] = 0x80;
-	h[21] = h[22] = h[23] = h[24] = h[25] = h[26] = 0;
-	//((uint8_t*)h)[80] = 0x80;
-	//((uint8_t*)h)[128-17] = 0x02;
-	//((uint8_t*)h)[128-16] = 0x80;
-	//((uint8_t*)h)[128-15] = 0x02;
+	h[21] =
+  h[22] =
+  h[23] =
+  h[24] =
+  h[25] =
+  h[26] = 0;
 	h[27] = 0x2000000;
 	h[28] = 0x280;
-	//h[29] = h[30] = h[31] = 0;
 
 	uint32_t k0 = 640; // bitlen
-	uint32_t W[64];
+	__shared__ uint32_t W[64];
 
 	#pragma unroll 8
 	for (int i = 0; i < 32; i+=4) {
@@ -140,6 +137,7 @@ void cuda_echo_round_80(uint32_t *const __restrict__ sharedMemory, uint32_t *con
 	#pragma unroll
 	for (int i = 61; i<64; i++) W[i] = 0;
 
+	#pragma unroll
 	for (int i = 0; i < 10; i++)
 		echo_round(sharedMemory, W, k0);
 
@@ -156,18 +154,14 @@ void cuda_echo_round_80(uint32_t *const __restrict__ sharedMemory, uint32_t *con
 __device__ __forceinline__
 void echo_gpu_init(uint32_t *const __restrict__ sharedMemory)
 {
-	/* each thread startup will fill a uint32 */
-	if (threadIdx.x < 128) {
-		sharedMemory[threadIdx.x] = d_AES0[threadIdx.x];
-		sharedMemory[threadIdx.x + 256] = d_AES1[threadIdx.x];
-		sharedMemory[threadIdx.x + 512] = d_AES2[threadIdx.x];
-		sharedMemory[threadIdx.x + 768] = d_AES3[threadIdx.x];
-
-		sharedMemory[threadIdx.x + 64 * 2] = d_AES0[threadIdx.x + 64 * 2];
-		sharedMemory[threadIdx.x + 64 * 2 + 256] = d_AES1[threadIdx.x + 64 * 2];
-		sharedMemory[threadIdx.x + 64 * 2 + 512] = d_AES2[threadIdx.x + 64 * 2];
-		sharedMemory[threadIdx.x + 64 * 2 + 768] = d_AES3[threadIdx.x + 64 * 2];
-	}
+  sharedMemory[threadIdx.x] = d_AES0[threadIdx.x];
+  sharedMemory[threadIdx.x + 128] = d_AES0[threadIdx.x + 128];
+  sharedMemory[threadIdx.x + 256] = d_AES1[threadIdx.x];
+  sharedMemory[threadIdx.x + 384] = d_AES1[threadIdx.x + 128];
+  sharedMemory[threadIdx.x + 512] = d_AES2[threadIdx.x];
+  sharedMemory[threadIdx.x + 640] = d_AES2[threadIdx.x + 128];
+  sharedMemory[threadIdx.x + 768] = d_AES3[threadIdx.x];
+  sharedMemory[threadIdx.x + 896] = d_AES3[threadIdx.x + 128];
 }
 
 __host__
